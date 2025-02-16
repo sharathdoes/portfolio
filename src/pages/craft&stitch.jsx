44f670a83bf63f932283299f772f9d2b10dd2e3a
sharath g <152028994+sharathdoes@users.noreply.github.com>
2025-02-16 16:10:30 +0530

    Update craft&stitch.jsx

--- src/pages/craft&stitch.jsx
+++ src/pages/craft&stitch.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Github } from 'lucide-react';
+import image from './image.png'; // Import the image
 
 function cands() {
   return (
     <div>
-      <h2>Mern stack Project</h2>
-      <p className='mb-2 mt-2'>
-      An application to create opportunities for designers all over world by connecting them to source managers of clothing companies.  </p>    <p>
-        Check out the projects on my  <a href="https://github.com/sharathdoes/Craft-and-stitch-server" target="_blank" rel="noopener noreferrer" className="inline-flex items-center">
+      <h2 className='mb-4 font-bold '>Designathon - Hackathon</h2>
+      <a href={image} target="_blank" rel="noopener noreferrer">
+        <img className="rounded-lg mb-2" src={image} alt="Designathon Project Image" />
+      </a> {/* Display the image with a link to open it */}
+      <p className='mb-2 mt-4'>
+      The Designathon Hackathon, hosted at VNR VJIET, was a thrilling event with a prize pool of 1 lakh.  </p>    <p>
+        <a href="https://github.com/sharathdoes/Designathon-2k25" target="_blank" rel="noopener noreferrer" className="inline-flex items-center">
           <Github className="w-4 h-4 mr-1" />
           GitHub
+        </a> | <a href="https://designathon-2k25-vnr.vercel.app" target="_blank" rel="noopener noreferrer" className="inline-flex items-center">
+          Live
         </a>.
       </p>
     </div>
